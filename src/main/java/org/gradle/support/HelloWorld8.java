911f1f67d575c65cbead0345b8f7b2ef03e603d9
Kon Soulianidis <kon@outrospective.org>
2014-09-25 01:17:18 -0700

    Initial commit

---
+++ src/main/java/org/gradle/support/HelloWorld8.java
@@ -0,0 +1,11 @@
+package org.gradle.support;
+
+import java.util.function.Supplier;
+
+public class HelloWorld8 {
+	
+	public static void main(String[] args) {
+		Supplier<String> helloer = () -> "Hello World" ;
+		System.out.println(helloer.get());
+	}
+}
