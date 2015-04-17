75ce515ee5ecf09074d15d05e9d5d59bf94dcad6
iluxa-com <iluxa_com@mail.ru>
2015-04-17 18:40:16 +0300

    !!

--- src/main/java/org/gradle/support/HelloWorld8.java
+++ src/main/java/org/gradle/support/HelloWorld8.java
@@ -5,7 +5,7 @@ import java.util.function.Supplier;
 public class HelloWorld8 {
 	
 	public static void main(String[] args) {
-		Supplier<String> helloer = () -> "Hello World" ;
+		Supplier<String> helloer = () -> "Hello World!!!!" ;
 		System.out.println(helloer.get());
 	}
 }
